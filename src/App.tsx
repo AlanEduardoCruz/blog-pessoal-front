b7ababc962b9e9daadb145338601de00f16025e1
Alan <alan-hd@hotmail.com>
2024-04-29 16:27:54 -0300

    Implementando rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,37 @@
-import Footer from "./components/Footer/Footer";
-import Navbar from "./components/NavBar/NavBar";
-import Home from "./paginas/home/Home";
+import React from 'react';
+import './App.css';
+import Home from './paginas/home/Home'; // Importa o componente Home
+import Navbar from './components/NavBar/NavBar'; // Importa o componente Navbar
+import Footer from './components/Footer/Footer'; // Importa o componente Footer
+import { BrowserRouter, Route, Routes } from 'react-router-dom'; // Importa BrowserRouter, Route e Routes de react-router-dom
+import Login from './paginas/login/login'; // Importa o componente Login
 
 function App() {
   return (
     <>
-
-
-      <Navbar />
-      <Home />
-      <Footer />
-
-
+      {/* Define o BrowserRouter como o componente pai para gerenciar as rotas */}
+      <BrowserRouter>
+        {/* Renderiza o componente Navbar */}
+        <Navbar />
+        
+        {/* Define uma div com uma classe específica */}
+        <div className='min-h-[80vh]'>
+          {/* Define as rotas do aplicativo */}
+          <Routes>
+            {/* Define a rota para a página inicial, renderizando o componente Home */}
+            <Route path="/" element={<Home />} />
+            
+            {/* Define a rota para a página de login, renderizando o componente Login */}
+            <Route path="/login" element={<Login />} />
+            
+            {/* Define a rota para a página inicial, renderizando o componente Home */}
+            <Route path="/home" element={<Home />} />
+          </Routes>
+        </div>
+        
+        {/* Renderiza o componente Footer */}
+        <Footer />
+      </BrowserRouter>
     </>
   );
 }
