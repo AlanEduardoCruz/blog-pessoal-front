39c5428ad4ce50cf7b5a777308af808a729f5183
Alan <alan-hd@hotmail.com>
2024-05-09 14:22:49 -0300

    Ajustando as Pastas

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,3 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import 'react-toastify/dist/ReactToastify.css';
 import Login from './pages/login/Login';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
